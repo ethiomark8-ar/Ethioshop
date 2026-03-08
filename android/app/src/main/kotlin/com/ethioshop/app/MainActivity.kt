9256705838b8ba6f75e4522de9e887a159bacd08
ETHIO MARK8 <ethiomark8@gmail.com>
2026-03-08 17:04:28 +0000

    chore: wipe main branch, keeping .github and git.zip

--- android/app/src/main/kotlin/com/ethioshop/app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.ethioshop.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/ethioshop/ethioshop_merged/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.ethioshop.ethioshop_merged
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
